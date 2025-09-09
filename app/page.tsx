51359c17c00d1c271d68fc8963abfd5edb50227e
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-10 00:12:58 +0330

    fix: use single prismaClient

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
-import {PrismaClient} from "@prisma/client";
-
-const prisma = new PrismaClient();
+import {prisma} from "@/lib/prisma";
 
 export default async function Home() {
 
