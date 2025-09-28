e66f996afe49fc0d32abe3d22d3506dccb2700f7
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-28 18:52:58 +0330

    chore: Get form data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,11 @@
 "use server" 
 
-export async function createPost(formData: FormData) {}
+export async function createPost(formData: FormData) {
+    const rawFormData = {
+        tag: formData.get('tag'),
+        title: formData.get('title'),
+        content: formData.get('content'),
+        publishStatus: formData.get('publishStatus'),
+    };
+    
+}
