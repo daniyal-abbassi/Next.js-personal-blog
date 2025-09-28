bf3d207bcf1936ecb781f9fa704693babf5116c7
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-28 20:13:38 +0330

    feat: Add post to database serverAction implented

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,33 @@
-"use server" 
+"use server";
+import { z } from "zod";
+import {prisma} from "@/app/lib/prisma";
+
+const FormSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+  author_id: z.number(),
+  isPublished: z.boolean(),
+  tag_id: z.coerce.number(),
+});
+
+const CreatePost = FormSchema;
 
 export async function createPost(formData: FormData) {
-    const rawFormData = {
-        tag: formData.get('tag'),
-        title: formData.get('title'),
-        content: formData.get('content'),
-        publishStatus: formData.get('publishStatus'),
-    };
-    
+    const {author_id, tag_id, title, content, isPublished} = CreatePost.parse({
+        author_id: 1,
+        tag_id: formData.get("tag"),
+        title: formData.get("title"),
+        content: formData.get("content"),
+        isPublished: formData.get("publishStatus") === 'publish' ? true : false,
+    })
+  await prisma.post.create({
+    data: {
+        title: title,
+        content: content,
+        author_id: author_id,
+        tag_id: tag_id,
+        isPublished: isPublished,
+        updated_at: new Date(),
+    }
+  })
 }
