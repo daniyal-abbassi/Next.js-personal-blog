e4f71cd782f85c1b39b38c8b9e79ee84438ff3b1
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-28 04:30:17 +0330

    chore: fetch tags and pass to form

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,10 +1,13 @@
 import Form from "@/app/ui/create-form";
+import { getTags } from "../lib/data";
 
-export default function Page() {
+export default async function Page() {
+    const tags = await getTags();
+
     return (
         <div  className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
             <h2>creata post form</h2>
-            <Form />
+            <Form tags={tags}/>
         </div>
     )
 }
