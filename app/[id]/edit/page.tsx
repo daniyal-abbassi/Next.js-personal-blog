ffb7d7095bab0826b4c6efd5f7ea4d25b7c9a4e7
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-29 00:08:01 +0330

    chore: Access id in page

--- app/[id]/edit/page.tsx
+++ app/[id]/edit/page.tsx
@@ -1,7 +1,10 @@
-export default function Page() {
+export default async function Page(props: {params: Promise<{id: string}>}) {
+    const params = await props.params;
+    const id = params.id;
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h2>Edit post</h2>
+      <h2>Edit post with id: {id}</h2>
     </div>
   );
 }
