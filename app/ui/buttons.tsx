800b87fc29b28c3fe5c7f0c339e7814e260f0ad1
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-29 01:53:34 +0330

    chore: Add delete button

--- app/ui/buttons.tsx
+++ app/ui/buttons.tsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
 import { PencilIcon } from "@heroicons/react/24/outline";
+import { TrashIcon } from "@heroicons/react/24/outline";
 
 
 
 export  function EditPost({id}: {id : number}) {
     return (
-        <Link href={`/${id}/edit`} className="rounded-md border p-2 ">
+        <Link href={`/${id}/edit`} className="rounded-md border p-2 hover:border-blue-500">
             <PencilIcon className="w-5" />
         </Link>
+    )
+}
+
+export function DeletePost({id} : {id: number}) {
+    return (
+        <form>
+            <button type="submit" className="rounded-md border hover:border-red-500 p-2 cursor-pointer">
+                <span className="sr-only"> Delete</span>
+                <TrashIcon className="w-5" />
+            </button>
+        </form>
     )
 }
