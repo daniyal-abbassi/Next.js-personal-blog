5d45e43bff3c00a4384e419b891db3e82d6f59aa
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-29 00:43:40 +0330

    chore: Setup edit form with props

--- app/[id]/edit/page.tsx
+++ app/[id]/edit/page.tsx
@@ -1,10 +1,16 @@
+import { getPostById,getTags } from "@/app/lib/data";
+import Form from "@/app/ui/edit-form";
 export default async function Page(props: {params: Promise<{id: string}>}) {
     const params = await props.params;
     const id = params.id;
-
+    const [post,tags] = await Promise.all([
+        getPostById(id),
+        getTags(),
+    ]);
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h2>Edit post with id: {id}</h2>
+        <Form tags={tags} post={post} />
     </div>
   );
 }
