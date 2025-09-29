0eac0488d66e02b3b0b2ff950ba2259d0a7f18a8
daniyal-abbassi <slimshady3753@protonmail.com>
2025-09-30 02:30:55 +0330

    fix: use State type and handle zod error messages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,12 +4,28 @@ import {prisma} from "@/app/lib/prisma";
 import {revalidatePath} from 'next/cache';
 import { redirect } from "next/navigation";
 const FormSchema = z.object({
-  title: z.string(),
-  content: z.string(),
+  title: z.string({
+    error: 'A post cannot publish without a title.',
+  }),
+  content: z.string({
+    error: 'A post should contain something atleast!'
+  }),
   author_id: z.number(),
-  isPublished: z.boolean(),
-  tag_id: z.coerce.number(),
+  isPublished: z.boolean({
+    error: 'Publish it or not already?'
+  }),
+  tag_id: z.coerce.number().gt(0, {error: 'Should select a tag atleast!'}),
 });
+
+export type State = {
+    errors?: {
+        title?: string[];
+        content?: string[];
+        isPublished: string[];
+        tag_id: string[]
+    };
+    message?: string | null;
+}
 
 const CreatePost = FormSchema;
 
@@ -46,24 +62,38 @@ export async function editPost(id: number,formData: FormData) {
     revalidatePath('/');
     redirect('/');
 }
-export async function createPost(formData: FormData) {
-    const {author_id, tag_id, title, content, isPublished} = CreatePost.parse({
+export async function createPost(prevState: State,formData: FormData) {
+    const validatedFields = CreatePost.safeParse({ //this will return an object wheather success or error
         author_id: 1,
         tag_id: formData.get("tag"),
         title: formData.get("title"),
         content: formData.get("content"),
         isPublished: formData.get("publishStatus") === 'publish' ? true : false,
     })
-  await prisma.post.create({
-    data: {
-        title: title,
-        content: content,
-        author_id: author_id,
-        tag_id: tag_id,
-        isPublished: isPublished,
-        updated_at: new Date(),
+    if(!validatedFields.success) {
+        return {
+            error: validatedFields.error.flatten().fieldErrors,
+            message: 'Check the form inputs again, something is missing!!!'
+        }
     }
-  });
+    const {author_id, tag_id, title, content, isPublished} = validatedFields.data;
+    try {
+        await prisma.post.create({
+          data: {
+              title: title,
+              content: content,
+              author_id: author_id,
+              tag_id: tag_id,
+              isPublished: isPublished,
+              updated_at: new Date(),
+          }
+        });
+        
+    } catch (error) {
+        return {
+            message: 'Database error: Failed to create Post.'
+        }        
+    }
   revalidatePath('/');
   redirect('/');
 }
