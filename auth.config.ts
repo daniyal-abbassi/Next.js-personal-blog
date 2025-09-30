d81c7a1965dc6b66291e72e2b14519999d799396
daniyal-abbassi <slimshady3753@protonmail.com>
2025-10-01 02:52:54 +0330

    feat: Add nextAuth configuration

--- auth.config.ts
+++ auth.config.ts
@@ -4,5 +4,19 @@ export const authConfig = {
     pages: {
         signIn: '/login'
     },
-    
+    callbacks: {
+        authorized({auth, request: {nextUrl}}) { //auth: session | nextUrl: request
+            const isLoggedIn = !!auth?.user; // !! => return a boolean
+            const isOnCreate = !!nextUrl.pathname.startsWith('/create');
+            const isOnEdit = !!nextUrl.pathname.startsWith('/edit');
+            if(isOnCreate || isOnEdit) {
+                if (isLoggedIn) return true; //user is logged-in and also in create page.
+                return false; //user is not logged in => back to log-in page.
+            } else if(isLoggedIn) { //user is in create and also is logged-in, so stay(redirect) in /create page.
+                return Response.redirect(new URL('/create',nextUrl));
+            }
+            return true;
+        }
+    },
+    providers: [],
 } satisfies NextAuthConfig;
