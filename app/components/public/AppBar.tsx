6786388eea5f9615ea6caae8764cbd94f593faa9
daniyal-abbassi <slimshady3753@protonmail.com>
2025-10-07 05:54:51 +0330

    fix: Change form action

--- app/components/public/AppBar.tsx
+++ app/components/public/AppBar.tsx
@@ -1,9 +1,7 @@
 "use server";
-import { alpha } from "@mui/material/styles";
-import { styled } from "@mui/system";
+
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import ColorModeIconDropdown from "@/app/lib/theme/ColorModeIconDropdown";
@@ -11,22 +9,9 @@ import ColorModeIconDropdown from "@/app/lib/theme/ColorModeIconDropdown";
 import Link from "next/link";
 import { signOut, auth } from "@/auth";
 import MobileAppBar from "./MobileAppBar";
+import { StyledToolbar } from "@/app/ui/styledToolbar";
+import { handleSignOut } from "@/app/lib/actions";
 
-const StyledToolbar = styled(Toolbar)(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-  flexShrink: 0,
-  borderRadius: `calc(${theme.shape.borderRadius}px + 8px)`,
-  backdropFilter: "blur(24px)",
-  border: "1px solid",
-  borderColor: (theme.vars || theme).palette.divider,
-  backgroundColor: theme.vars
-    ? `rgba(${theme.vars.palette.background.defaultChannel} / 0.4)`
-    : alpha(theme.palette.background.default, 0.4),
-  boxShadow: (theme.vars || theme).shadows[1],
-  padding: "8px 12px",
-}));
 
 export default async function AppAppBar(/*{ isAuth, setIsAuth }*/) {
   const session = await auth();
@@ -81,10 +66,7 @@ export default async function AppAppBar(/*{ isAuth, setIsAuth }*/) {
               }}
             >
               <form
-                action={async () => {
-                  "use server";
-                  await signOut({ redirectTo: "/" });
-                }}
+                action={handleSignOut}
               >
                 <Button color="error" variant="outlined" size="small">
                   Log Out
