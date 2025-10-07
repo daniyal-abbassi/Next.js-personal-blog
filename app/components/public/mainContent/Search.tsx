0c011e232b05767f96921b19952b4f81b5bdc069
daniyal-abbassi <slimshady3753@protonmail.com>
2025-10-08 00:33:33 +0330

    fix: Use URL for search input

--- app/components/public/mainContent/Search.tsx
+++ app/components/public/mainContent/Search.tsx
@@ -1,10 +1,25 @@
+'use client';
 import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from 'use-debounce';
 
-
-export function Search({ handleSearch, searchTitles }) {
+export function Search() {
+    const searchParams = useSearchParams();
+    const pathname =usePathname();
+    const {replace} = useRouter();
+    const handleSearch = useDebouncedCallback((term:string)=>{
+        const params = new URLSearchParams(searchParams);
+        params.set('page','1');
+        if(term) {
+            params.set('search',term)
+        } else {
+            params.delete('search');
+        };
+        replace(`${pathname}?${params.toString()}`);
+    },500)
     return (
       <FormControl sx={{ width: { xs: "100%", md: "25ch" } }} variant="outlined">
         <OutlinedInput
@@ -12,8 +27,8 @@ export function Search({ handleSearch, searchTitles }) {
           id="search"
           placeholder="Search…"
           sx={{ flexGrow: 1 }}
-          onChange={handleSearch}
-          value={searchTitles}
+          onChange={(event) => handleSearch(event.target.value)}
+          defaultValue={searchParams.get('search')?.toString()}
           startAdornment={
             <InputAdornment position="start" sx={{ color: "text.primary" }}>
               <SearchRoundedIcon fontSize="small" />
