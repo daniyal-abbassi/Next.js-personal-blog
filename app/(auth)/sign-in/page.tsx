42035cf93f4e4dad5bacb2aebea6484a683dcb95
daniyal-abbassi <slimshady3753@protonmail.com>
2025-10-10 00:52:20 +0330

    fix: Delete dependencies

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,4 @@
-import ColorModeSelect from "@/app/lib/theme/ColorModeSelect";
 import LoginForm from "@/app/ui/login-form";
-import { SignInContainer,SignInCard } from "@/app/ui/styledThemes";
-import { Box, Button, Divider, FormControl, FormControlLabel, FormLabel, TextField, Typography } from "@mui/material";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default function Page() {
