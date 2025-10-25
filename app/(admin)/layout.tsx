f4b5030ccffa71c66548363e8c4d4e06e7db0e9f
daniyal-abbassi <slimshady3753@protonmail.com>
2025-10-25 13:29:32 +0330

    feat: Add next-theme and shadcn configuration prerequisites

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,25 @@
+import { ThemeProvider } from "@/app/lib/theme/theme-provider";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  );
+}
